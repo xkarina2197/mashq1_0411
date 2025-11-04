c178c4cf6cbe53766a392cd2d954814228a9def1
xkarina2197 <xkarina2197@gmail.com>
2025-11-04 03:56:46 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+# 1-misol
+summa = 0
+kop = 0
+
+for i in  range(1,10):
+    summa += i
+    kop *= i
+print(summa)
+print(kop)
+
+
+# 2-misol
+sum = 0
+kop = 0
+
+for i in range(5, 16):
+    sum += i
+    kop *= i
+print(kop)
+print(sum)
+
+# 3-misol
+n = int(input("n  soni kirit: "))
+kop = 0
+sum = 0
+
+for  i in range(1, n+1):
+    kop *= i
+    sum += i
+print(kop)
+print(sum)
+
+# 4-misol
+sum = 0
+kop = 0
+
+for i in range(1, 7):
+    sum += i
+    kop *= i
+print(kop)
+print(sum)
+
+# 5-misol
+n = int(input("n soni kirit: "))
+kop = 0
+sum = 0
+
+for  i in range(1, n+1):
+    kop *= i
+    sum += i
+print(kop)
+print(sum)
